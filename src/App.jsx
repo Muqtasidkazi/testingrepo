376ce491d7e748e632deb1ab38e68174d6154984
Muqtasidkazi <kazimuqtasid@gmail.com>
2024-05-30 23:30:37 -1200

    changes in scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
-import Pdfconvertor from "./components/Pdfconvertor";
 
 function App() {
   const [elmts] = useState([
@@ -10,45 +9,78 @@ function App() {
     { title: "4", desc: "desc1" },
     { title: "5", desc: "desc1" },
     { title: "6", desc: "desc1" },
+    { title: "7", desc: "desc1" },
+    { title: "8", desc: "desc1" },
   ]);
 
   const sliderRef = useRef(null);
+  const [onPausedTeam, setOnPausedTeam] = useState(false);
 
-  // useEffect(() => {
-  //   const slider = sliderRef.current;
-  //   let scrollAmount = 0;
-  //   const autoScroll = () => {
-  //     if (slider) {
-  //       scrollAmount += 234;
-  //       console.log(slider.clientWidth,'client')
-  //       console.log(slider.scrollWidth)
-  //       slider.scrollLeft = scrollAmount;
-  //       if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
-  //         slider.scrollLeft = 0; 
-  //         scrollAmount = 0;
-  //       }
-  //     }
-  //   };
-  //   const interval = setInterval(autoScroll, 1000); 
-
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  function handleLeftArrow() {
+  const handleLeftHorizontalScroll = (scrollOffset, sliderRef) => {
     if (sliderRef.current) {
-      sliderRef.current.scrollLeft -=sliderRef.current.clientWidth; // Adjust scroll value as needed
+      sliderRef.current.scrollLeft += scrollOffset;
     }
-  }
+  };
 
-  function handleRightArrow() {
+  // const [first, setfirst] = useState(second)
+  let scrollAmount = 0;
+  const handleRightHorizontalScroll = (scrollOffset, sliderRef) => {
+    const TeamSlider = sliderRef.current;
+    scrollAmount += scrollOffset;
     if (sliderRef.current) {
-      sliderRef.current.scrollLeft +=sliderRef.current.clientWidth; // Adjust scroll value as needed
+      console.log(scrollAmount);
+      sliderRef.current.scrollLeft += scrollAmount;
+      console.log(scrollAmount, "scrollAmount");
+    } 
+    if(scrollAmount >= TeamSlider.scrollWidth - TeamSlider.clientWidth){
+      sliderRef.current.scrollLeft = 0;
     }
-  }
+  };
+
+  const handleMouseEnter = () => {
+    setOnPausedTeam(true);
+  };
+
+  const handleMouseLeave = () => {
+    setOnPausedTeam(false);
+  };
+
+  useEffect(() => {
+    let screenWidth = window.screen.width;
+    const TeamSlider = sliderRef.current;
+    let scrollAmount = 0;
+
+    const autoScroll = () => {
+      if (TeamSlider && !onPausedTeam && screenWidth > 991) {
+        const itemWidth = TeamSlider.querySelector(".slider-item").clientWidth;
+        scrollAmount += itemWidth;
+        console.log(scrollAmount);
+        TeamSlider.scrollLeft = scrollAmount;
+        if (scrollAmount >= TeamSlider.scrollWidth - TeamSlider.clientWidth) {
+          sliderRef.current.scrollLeft += 380;
+          setTimeout(() => {
+            TeamSlider.scrollLeft = 0;
+
+            scrollAmount = 0;
+          }, 1500);
+        }
+      }
+    };
+    const interval = setInterval(autoScroll, 1500);
+
+    return () => clearInterval(interval);
+  }, [onPausedTeam]);
 
   return (
-    <div className="slider-body">
-      <button className="btn" onClick={handleLeftArrow}>
+    <div
+      className="slider-body"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <button
+        className="btn"
+        onClick={() => handleLeftHorizontalScroll(-380, sliderRef)}
+      >
         left
       </button>
       <div className="slider-container" ref={sliderRef}>
@@ -58,11 +90,12 @@ function App() {
           </div>
         ))}
       </div>
-      <button className="btn" onClick={handleRightArrow}>
+      <button
+        className="btn"
+        onClick={() => handleRightHorizontalScroll(380, sliderRef)}
+      >
         right
       </button>
-
-      <Pdfconvertor/>
     </div>
   );
 }
